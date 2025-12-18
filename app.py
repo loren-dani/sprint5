cfe750a886074d09f0d867b8135547ae1126c23c
loren-dani <loren_danielle@hotmail.com>
2025-12-18 17:31:30 -0300

    edições finais do projeto

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.header('Jogando uma moeda')
+car_data = pd.read_csv('vehicles.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
 
-st.write('Ainda não é um aplicativo funcional. Em construção.')
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    
+    # criar um histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
