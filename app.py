f459f8672e6cbc7b12e4b4a580a96addf39998f0
loren-dani <36082194+loren-dani@users.noreply.github.com>
2025-12-23 16:48:19 -0300

    Add scatter plot button and functionality

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ st.header('Venda de Veículos nos EUA: dados')
 car_data = pd.read_csv('vehicles.csv') # lendo os dados
 hist_button = st.button('Criar histograma') # criar um botão
 
+dist_button = st.button('Criar gráfico de dispersão') # botão para gráfico dispersão
+
 if hist_button: # se o botão for clicado
     # escrever uma mensagem
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
@@ -19,15 +21,18 @@ if hist_button: # se o botão for clicado
     )
 
     st.plotly_chart(hist_fig, use_container_width=True)
+
+ if dist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um gráfico de dispersão')
     
-    #criar gráfico de dispersão
-    hist_fig = px.histogram(
+     #criar gráfico de dispersão
+    dist_fig = px.scatter(
         car_data,
         x="odometer",
-        title="Distribuição da Quilometragem"
+        y='price',
+        title="Distribuição da Quilometragem por Preço"
     )
 
-    st.plotly_chart(hist_fig, use_container_width=True)
-
-    # exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(dist_fig, use_container_width=True)
+   
